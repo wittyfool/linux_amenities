bef286a6ef16d97d13a0bbe2a6c8ada1efc1e606
nobu <wittyfool@github.com>
2015-06-25 19:18:36 +0900

    ls2html

---
+++ ls2html.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+$dir = shift // '.';
+
+if(opendir(DIR, $dir)){
+  @files = readdir(DIR);
+  closedir(DIR);
+}
+
+#print join(", ", @files);
+#print "\n";
+
+print <<_HTML;
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<pre>
+
+_HTML
+
+print "[$dir]\n";
+for $f (@files){
+  next if ($f =~ /^\./);
+  print qq|<a href=$dir/$f"> $f </a>\n|;
+}
+
+print <<_HTML;
+</pre>
+</body>
+</html>
+_HTML
