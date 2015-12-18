be3dcf11e6697892e1a4339c338e21242fbd90d2
nobu <nobu@mokuren.(none)>
2015-12-18 18:57:23 +0900

    bug fix

--- giftools/gifcaption.c
+++ giftools/gifcaption.c
@@ -84,8 +84,10 @@ main(int argc, char *argv[]){
     bg = gdImageColorClosest(im, 255,255,255);
   }
 
-  x = 20;
-  y = 20;
+  // x = 20;
+  // y = 20;
+  x = ORIGIN_X;
+  y = ORIGIN_Y;
   for(i=3; i<argc; i++){
     int option=1;
     if(option && (argv[i][0] == '-')){
