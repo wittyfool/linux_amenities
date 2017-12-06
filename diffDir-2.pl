4fa62936d21d6cc9fff36e862053e3fa1bbe1c94
土屋 <nobu.tsuchiya@gmail.com>
2017-12-06 10:55:17 +0900

    etc

---
+++ diffDir-2.pl
@@ -0,0 +1,100 @@
+#! /usr/bin/perl -s
+#    eval 'exec /usr/bin/perl -S $0 ${1+"$@"}'
+#        if 0; #$running_under_some_shell
+
+$|=1;
+my $count = 0;
+
+my $arg = {
+  debug => $debug,
+};
+
+use strict;
+use File::Find ();
+use Data::Dumper;
+
+my $dir1 = shift || die;
+my $dir2 = shift || die;
+my $hash1 = {};
+my $hash2 = {};
+my $hash;
+my $start = `pwd | chop`;
+
+# Set the variable $File::Find::dont_use_nlink if you're using AFS,
+# since AFS cheats.
+
+# for the convenience of &wanted calls, including -eval statements:
+use vars qw/*name *dir *prune/;
+*name   = *File::Find::name;
+*dir    = *File::Find::dir;
+*prune  = *File::Find::prune;
+
+sub wanted;
+
+
+
+# Traverse desired filesystems
+$hash = $hash1;
+File::Find::find({wanted => \&wanted}, $dir1);
+
+$hash = $hash2;
+File::Find::find({wanted => \&wanted}, $dir2);
+
+print Dumper $hash1 if $arg->{debug};
+print Dumper $hash2 if $arg->{debug};
+
+my $kk;
+
+for my $k (sort keys %$hash1){
+  ($kk = $k ) =~ s/^$dir1/$dir2/;
+
+  if( !defined($hash2->{$kk})){
+    print " [$k] not found in [$dir2]\n";
+    next;
+  }
+  if( $hash1->{ $k } ne $hash2->{ $kk }){
+    print " [$k] differ \n";
+  }
+}
+
+exit;
+
+sub report(){
+  $count++;
+
+  print ".";
+
+  if(($count % 50)==0){
+    print "[$count]\n";
+  }
+}
+
+
+sub wanted {
+    my ($dev,$ino,$mode,$nlink,$uid,$gid);
+    my ($full, $sum);
+
+    report();
+
+    (($dev,$ino,$mode,$nlink,$uid,$gid) = lstat($_)) &&
+    do {
+      print("wanted: $name\n") if($arg->{debug});
+    #
+      if($name =~ /^\//){
+        $full = $name;
+      } else {
+        $full = $start.'/'.$name;
+      }
+
+	if( -f $full){
+          $sum = `sum $full 2>/dev/null`;
+	  $sum =~ s/[\r\n]//;
+          $hash->{ $name } = $sum;
+	  #print "dir[$dir] name[$name] : sum[$sum]\n";
+	} else {
+	  print "Not file?? [$start][$name][$dir]\n" if($arg->{debug});
+        }
+      ;
+    };
+}
+
