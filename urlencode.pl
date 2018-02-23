a5bdda65a45acef403194e42ee86fc95890e0383
Nobushige Tsuchiya <nobu.tsuchiya@gmail.com>
2018-02-23 11:41:54 +0900

    encodeいらない

--- urlencode.pl
+++ urlencode.pl
@@ -5,7 +5,7 @@ use Encode;
 while(<>){
   $str .= $_;
 }
-$str = encode_utf8($str);
+#$str = encode_utf8($str);
 $str =~ s/([^ 0-9a-zA-Z])/"%".uc(unpack("H2",$1))/eg;
 $str =~ s/ /+/g;
 print($str);
