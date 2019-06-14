de49008ead447a2c6d65fe6afe64fd405b4f1dae
Nobushige Tsuchiya <nobu.tsuchiya@gmail.com>
2019-06-14 15:26:02 +0900

    stdoutに出力

--- xls2csv.py
+++ xls2csv.py
@@ -6,23 +6,24 @@ import csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument("<xlsfile>", help="specify xls file", type=str)
-
 parser.parse_args()
 
 #print( sys.argv[0] )
-print( sys.argv[1] )
+print( "Reading xlsfile: " + sys.argv[1] )
 
 def csv_from_excel():
     wb = xlrd.open_workbook( sys.argv[1] )
     sheet = wb.sheet_by_index(0)
-    your_csv_file = open('dst.csv', 'w', encoding='utf8')
-    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+
+    # csv_file = open('dst.csv', 'w', encoding='utf8')
+    csv_file = sys.stdout
+
+    wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
     for row_index in range(sheet.nrows):
-        #row = sheet.row(row_index)
-        #print("row idx=" + str(row_index))
         wr.writerow(sheet.row_values(row_index))
 
-    your_csv_file.close()
+    if csv_file is not sys.stdout:
+        csv_file.close()
 
 csv_from_excel()
