459a5847a949bc4a03dc4e752315e42f56ff67a0
Nobushige Tsuchiya <nobu.tsuchiya@gmail.com>
2019-08-25 12:05:28 +0900

    google chat にポストするツール

---
+++ postHangout.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -s
+#
+$url = `grep url postHangout.config | cut -f 2`;
+$token = `grep token postHangout.config | cut -f 2`;
+$key = `grep key postHangout.config | cut -f 2`;
+$room = `grep room postHangout.config | cut -f 2`;
+$thread = `grep room postHangout.config | cut -f 2`;
+
+$url =~ s/[\r\n]+$//;
+$token =~ s/[\r\n]+$//;
+$key =~ s/[\r\n]+$//;
+$room =~ s/[\r\n]+$//;
+#
+$cmd = "curl -s -m 5 -H 'Content-Type:application/json' -d \@- $url\\?key=$key\\&token=$token";
+$pipe  = "curl -s -m 5 -H 'Content-Type:application/json' -d \@- $url\\?key=$key\\&token=$token";
+
+print $cmd;
+print "\n";
+
+while(<>){
+	s/[\r\n]+$//;
+	$msg .= $_;
+	$msg .= "\\n\n";
+};
+
+if($exec){
+	$date = `date`;
+
+	open(PIPE, '|-', $pipe) || die;
+	print PIPE <<_PIPE;
+{ 
+ "thread": { "name": "spaces/$room/threads/$thread" }, 
+ "text" : "$msg"
+}
+_PIPE
+	close(PIPE);
+	
+}
