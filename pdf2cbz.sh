5375aefb35d0dfb8773060be8a9859e73d54cba6
Nobu Tsuchiya <nobu.tsuchiya@gmail.com>
2023-02-04 18:48:39 +0900

    show progress while converting

--- pdf2cbz.sh
+++ pdf2cbz.sh
@@ -5,6 +5,12 @@ tmpdir="/var/tmp"
 
 mkdir -p $tmpdir/jpeg
 
-pdftoppm -f 1 -l 900 -jpeg $file $tmpdir/jpeg/pages
+pdftoppm -q -f 1 -l 900 -jpeg $file $tmpdir/jpeg/pages & 
+
+sleep 1
+while [[ -n $(jobs -r) ]]; do ls -ltr $tmpdir/jpeg | tail -1; sleep 1; done
+
+wait
+
 zip $base.cbz $tmpdir/jpeg/pages-*.jpg
 rm $tmpdir/jpeg/pages-*.jpg
