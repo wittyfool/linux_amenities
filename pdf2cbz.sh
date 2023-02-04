09e1dce723d85e8f4bd51619d1a3e0b7a26eaaf8
Nobu Tsuchiya <nobu.tsuchiya@gmail.com>
2023-02-04 11:56:05 +0900

    pdftoppm

--- pdf2cbz.sh
+++ pdf2cbz.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 file=$1
 base="$(basename -s .pdf $file)"
+tmpdir="/var/tmp"
 
-pdfimages -f 1 -l 900 -j $file jpeg/pages
-zip $base.cbz jpeg/pages-*.jpg
-rm jpeg/pages-*.jpg
+mkdir -p $tmpdir/jpeg
+
+pdftoppm -f 1 -l 900 -jpeg $file $tmpdir/jpeg/pages
+zip $base.cbz $tmpdir/jpeg/pages-*.jpg
+rm $tmpdir/jpeg/pages-*.jpg
